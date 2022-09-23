08254ca4413b7234242d17967a60b3dbe3a9f82b
Masuduzzaman Mohammad <masuduzzaman_811@outlook.com>
2022-09-23 12:17:17 +0600

    add linter config and fix most of the errors

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import styles from "./TodoItem.module.css";
+import styles from './TodoItem.module.css';
 
 export default class TodoItem extends Component {
   render() {
     return (
       <li className={styles.item}>
-        <input type="checkbox" className={styles.checkbox} checked={this.props.todo.completed} onChange={() => this.props.handleChangeProps(this.props.todo.id)} /> {this.props.todo.title}
-        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)} >Delete</button>
-       
+        <input type="checkbox" className={styles.checkbox} checked={this.props.todo.completed} onChange={() => this.props.handleChangeProps(this.props.todo.id)} />
+        {' '}
+        {this.props.todo.title}
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
+
       </li>
-     
-    )
+
+    );
   }
 }
